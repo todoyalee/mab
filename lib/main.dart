d585278b69a6a1a03c4280e427086a1efbdc78e9
Mohamed Ali Belkouri <medali.b@devnet.me>
2024-07-06 02:59:27 +0100

    feat:adding_animation_to_login

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+
+import 'package:mab/views/sign_in.dart';
+void main() {
+  runApp( signIn());
+}
+
---
+++ lib/views/sign_in.dart
@@ -0,0 +1,209 @@
+import 'package:flutter/material.dart';
+
+import 'package:animate_do/animate_do.dart';
+
+class signIn extends StatefulWidget {
+  const signIn({super.key});
+
+  @override
+  State<signIn> createState() => _signInState();
+}
+
+class _signInState extends State<signIn> {
+  @override
+  Widget build(BuildContext context) {
+    return 
+
+    MaterialApp(
+      home:
+    
+    
+    Scaffold(
+      //backgroundColor: Colors.white
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              const SizedBox(
+                height: 150
+              ),
+              Container(
+                height: 150,
+                child: Stack(
+                  children: <Widget>[
+                    Positioned(
+                      left: 30,
+                      width: 300,
+                      height: 87,
+                      child: FadeInUp(
+                        duration: const Duration(seconds: 1),
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage('images/back.png'),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      left: 140,
+                      width: 80,
+                      height: 150,
+                      child: FadeInUp(
+                        duration: Duration(milliseconds: 1200),
+                        child: Container(
+                            
+                            ),
+                      ),
+                    ),
+                    Positioned(
+                      right: 40,
+                      top: 40,
+                      width: 80,
+                      height: 150,
+                      child: FadeInUp(
+                        duration: Duration(milliseconds: 1300),
+                        child: Container(
+                          
+                            ),
+                      ),
+                    ),
+                    Positioned(
+                      child: FadeInUp(
+                        duration: Duration(milliseconds: 1600),
+                        child: Container(
+                          margin: EdgeInsets.only(top: 50),
+                          child: const Center(
+                            child: Text(
+                              "Login",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 40,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(30.0),
+                child: Column(
+                  children: <Widget>[
+                    FadeInUp(
+                      duration: Duration(milliseconds: 1800),
+                      child: Container(
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(
+                            color: Color.fromRGBO(143, 148, 251, 1),
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color.fromRGBO(143, 148, 251, .2),
+                              blurRadius: 20.0,
+                              offset: Offset(0, 10),
+                            ),
+                          ],
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.all(8.0),
+                              decoration: BoxDecoration(
+                                border: Border(
+                                  bottom: BorderSide(
+                                    color: Color.fromRGBO(143, 148, 251, 1),
+                                  ),
+                                ),
+                              ),
+                              child: TextField(
+                                
+                               // controller: emailFieldController,
+                               
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: "Email",
+                                  hintStyle: TextStyle(color: Colors.grey[700]),
+                                  prefixIcon: Icon(Icons.email,
+                                      color: Color(
+                                          0xFFD816B4)), 
+                                ),
+                              ),
+
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(8.0),
+                              child: TextField(
+                               // controller: passFieldController,
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: "Password",
+                                  hintStyle: TextStyle(color: Colors.grey[700]),
+                                  prefixIcon: Icon(Icons.lock,
+                                      color: Color(
+                                          0xFFD816B4)), 
+                                ),
+                              ),
+                            ),
+
+                           
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    FadeInUp(
+                      duration: Duration(milliseconds: 1900),
+                      child: GestureDetector(
+                        onTap: () {
+                          //login(); // Call your login function here
+                        },
+                        child: Container(
+                          height: 50,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            gradient: LinearGradient(
+                              colors: [
+                                Color.fromRGBO(143, 148, 251, 1),
+                                Color(
+                                    0xFF353EE5),
+                              ],
+                            ),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Login",
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 70,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+}
